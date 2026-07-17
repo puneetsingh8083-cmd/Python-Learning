ce48cd473f239616ff16be295b0226050cf33279
puneetsingh8083-cmd <puneetsingh8083@gmail.com>
2026-07-17 18:39:15 +0530

    Added python variables

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,8 @@
+age = 34 # integer
+name = "Harry" # string
+cgpa = 9.8 # float
+
+# Variable names can contain letters, numbers, and underscores.
+# Variable names must start with a letter or underscore.
+# Variable names are case-sensitive.
+# Avoid using Python keywords as variable names (e.g., print, if , else)
---
+++ 03_variables/02_data_types.py
@@ -0,0 +1,23 @@
+# Python supports several built-in data types:
+# integers (int): Whole numbers (e.g., 10 , -5).
+# Floats (float): Decimal numbers (e.g., 3.14 , -0.001).
+# Strings (str): Text data enclosed in quotes (e.g., "Hello" , 'Python').
+# Booleans (bool): Represents True or False .
+# Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+# Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+# Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+# Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 9.8
+print(cgpa)
+
+name ="Harry"
+print(name)
+is_complete = True # can also be False
+print(is_complete)
+print(type(is_complete))

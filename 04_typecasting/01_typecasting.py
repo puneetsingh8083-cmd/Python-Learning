46df9f899c0fa536526abda3a9d2305d2eabd77e
puneetsingh8083-cmd <puneetsingh8083@gmail.com>
2026-07-19 21:13:31 +0530

    Added python typecasting

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,22 @@
+# a =34 
+# print(a)
+# b ="34"
+# print(b)
+# print(type(b))
+
+# d = 223
+# print(d)
+# print(type(d))
+
+# e = str(d)
+# print(e)
+# print(type(e))
+
+# c = int(b)
+# print(c)
+# print(type(c))
+
+pi = 3.14
+pi_int =int(pi)
+print(pi_int)
+
